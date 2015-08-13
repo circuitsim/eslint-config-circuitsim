e168762d27af40579dc025532d73dfb0980acd21
Thom Wright <github@thomwright.co.uk>
2015-08-13 13:52:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#  eslint-config-circuitsim
+[![npm](https://img.shields.io/npm/v/thom-eslint.svg?style=flat-square)](https://www.npmjs.com/package/thom-eslint)
+[![David](https://img.shields.io/david/dev/ThomWright/thom-eslint.svg?style=flat-square)](https://david-dm.org/ThomWright/thom-eslint#info=peerDependencies)
+
+ESLint settings for CircuitSim
---
+++ react.js
@@ -0,0 +1,29 @@
+var defaultConfig = require('./index.js');
+
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "./index.js",
+    "rules": {
+      "react/jsx-boolean-value": 1,
+      "react/jsx-quotes": 1,
+      "react/jsx-no-undef": 1,
+      "react/jsx-uses-react": 1,
+      "react/jsx-uses-vars": 1,
+      "react/no-did-mount-set-state": 1,
+      "react/no-did-update-set-state": 1,
+      "react/no-multi-comp": 1,
+      "react/no-unknown-property": 1,
+      "react/prop-types": 1,
+      "react/react-in-jsx-scope": 1,
+      "react/self-closing-comp": 1,
+      "react/wrap-multilines": 1
+    },
+    "ecmaFeatures": {
+        "jsx": true,
+        "spread": true,
+        "restParams": true
+    },
+    "plugins": [
+        "react"
+    ]
+};
